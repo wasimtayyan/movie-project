6d3e215410db66caee4d7aa995d213857f96adaf
wasim altayyan <wasimtayyan@gmail.com>
2023-05-20 17:20:26 +0300

    add search function

--- script.js
+++ script.js
@@ -38,20 +38,52 @@ const fetchMovie = async (movieId) => {
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
+  CONTAINER.innerHTML = ""
   movies.map((movie) => {
-    const movieDiv = document.createElement("div");
-    movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster">
-        <h3>${movie.title}</h3>
-        <h4>language : ${movie.original_language}</h4>`;
-    movieDiv.addEventListener("click", () => {
-      movieDetails(movie);
-    });
-    CONTAINER.appendChild(movieDiv);
+      const movieDiv = document.createElement("div");
+      movieDiv.innerHTML = ""
+      movieDiv.innerHTML = `
+        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title
+        } poster">
+        <h3>${movie.title}</h3>`;
+
+      movieDiv.addEventListener("click", () => {
+        movieDetails(movie);
+      });
+      CONTAINER.appendChild(movieDiv);
+    
+    
   });
 };
+// function renderMovieFromSearch(movies){
+//   CONTAINER.innerHTML = ""
+//   movies.map((movie) => {
+//     if(movie.backdrop_path !== null){
+//       const movieDiv = document.createElement("div");
+//       movieDiv.innerHTML = ""
+//       if (movie.media_type === "movie"){
+//         movieDiv.innerHTML = `
+//         <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title
+//           } poster">
+//         <h3>${movie.title}</h3>`;
+//       } else if (movie.media_type === "tv"){
+//         movieDiv.innerHTML = `
+//         <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title
+//           } poster">
+//         <h3>${movie.name}</h3>`;
+//       }
+      
+
+//       movieDiv.addEventListener("click", () => {
+//         movieDetails(movie);
+//       });
+//       CONTAINER.appendChild(movieDiv);
+//     }
+    
+
+
+//   });
+// }
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovie = (movie) => {
@@ -76,5 +108,24 @@ const renderMovie = (movie) => {
             <ul id="actors" class="list-unstyled"></ul>
     </div>`;
 };
+
+
+//search function 
+const search = document.getElementById("search")
+search.onkeyup = (e)=>{
+  searchInput(e.target.value)
+}
+
+function searchInput(value) {
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=6de312bb1131d8c5991b62ffbdfc1830&language=en-US&query=${value}&page=1&include_adult=false`)
+    .then(response => response.json())
+    // .then(data => console.log(data.results))
+    .then(data => renderMovies(data.results))
+    .catch(err => console.error(err));
+}
+
+//fetch genre
+
+
 
 document.addEventListener("DOMContentLoaded", autorun);
